7ffe18199fc345a0bb53b4be240b6bb8ae39770f
usama <usama.bsse3222@iiu.edu.pk>
2020-01-02 21:59:12 +0500

    routes updation

--- readme.md
+++ readme.md
@@ -1,20 +1,2 @@
-# How to run
+# Backend of the project : controllers , models, routes
 
-```
-npm install
-
-or
-
-yarn install
-```
-
-```
-npm run dev
-```
-
-Go to [locahost:3000](http://localhost:3000)
-
-This Node.js / Express app is set up using Mongoose / MongoDB.
-
-# node js part 4 completed
-<img src="pic.PNG">
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,7 +6,7 @@ const checkAuth = require('../middleware/check-auth');
 
 // router.get("/",UserController.sampleUser);
 router.get("/",UserController.getAll);
-//router.post("/login",UserController.loginUser);
+router.post("/login",UserController.loginUser);
 router.post("/register",UserController.registerUser);
 //router.get("/:_id",UserController.getSingleUser);
 // router.post("/",UserController.addUser);
